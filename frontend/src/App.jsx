25890d55f059f42609f75ef400c41f1835671c2e
CurlyCoding7 <shalu.s2021@gmail.com>
2024-02-27 15:10:20 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
+function App() {
+
+  return (
+<Router>
+    <div>
+      <header>
+         <Link to="/">amazona</Link>
+      </header>
+
+      <main>
+         <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:slug' element={<ProductScreen />} />
+
+         </Routes>
+      </main>
+
+    </div>
+    </Router>
+  )
+}
+
+export default App
